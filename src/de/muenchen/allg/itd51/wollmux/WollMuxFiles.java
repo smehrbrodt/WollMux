dfe4492a0242e8cee102f13588d8058ed8a8523d
Christian Köhler <christian.koehler@cib.de>
2015-01-29 15:10:28 +0100

    NoConfig Erweiterung. Sofern keine wollmux.conf gefunden wird wechselt wollmux in den NoConfig-Modus. Intern wird dabei das schema "noconfig" gesetzt. Beim Auftreten des ersten Fehlers, der seinen Grund in einer fehlenden wollmux.conf hat, wird einmalig ein Fehler ausgegeben.

--- src/de/muenchen/allg/itd51/wollmux/WollMuxFiles.java
+++ src/de/muenchen/allg/itd51/wollmux/WollMuxFiles.java
@@ -149,11 +149,10 @@ public class WollMuxFiles
    */
   private final static String WOLLMUX_CONF_PATH_VALUE_NAME = "ConfigPath";
 
-  private static final String WOLLMUX_CONF_NOT_FOUND_MESSAGE =
-    L.m("Es konnte keine WollMux-Konfiguration (wollmux.conf) gefunden werden!\n"
-      + "Die meisten WollMux-Funktionen werden daher nicht korrekt funktionieren.\n"
-      + "Es wurde erfolglos versucht die Datei wollmux.conf an folgenden Orten zu finden:\n");
+  private static final String WOLLMUX_NOCONF =
+      L.m("Es wurde keine WollMux-Konfiguration (wollmux.conf) gefunden - deshalb läuft WollMux im NoConfig-Modus.");
 
+  
   private static final long DATASOURCE_TIMEOUT = 10000;
 
   /**
@@ -260,14 +259,15 @@ public class WollMuxFiles
    * <li>unter dem Dateipfad, der in der Konstanten {@link #ETC_WOLLMUX_WOLLMUX_CONF}
    * festgelegt ist (nur Linux!)</li>
    * </ol>
-   * 
+   * @return true für den den Fall no Config, false bei gefundener wollmux.conf 
    * @author Matthias Benkmann (D-III-ITD 5.1)
    * @author Daniel Benkmann (D-III-ITD-D101)
    */
-  public static void setupWollMuxDir()
+  public static boolean setupWollMuxDir()
   {
     long time = System.currentTimeMillis(); // Zeitnahme fürs Debuggen
-
+    boolean noConf = false;                 // kein no conf mode
+    
     String userHome = System.getProperty("user.home");
     wollmuxDir = new File(userHome, ".wollmux");
 
@@ -399,8 +399,9 @@ public class WollMuxFiles
       }
     }
     else
-    { // wollmux.conf existiert nicht
-      Logger.error(WOLLMUX_CONF_NOT_FOUND_MESSAGE + searchPaths);
+    { // wollmux.conf existiert nicht - damit wechseln wir in den no config mode.
+      noConf = true;
+      Logger.log(WOLLMUX_NOCONF);
     }
 
     fido.dontBark();
@@ -443,6 +444,8 @@ public class WollMuxFiles
 
     Logger.debug(L.m(".wollmux init time: %1ms", ""
       + (System.currentTimeMillis() - time)));
+    
+    return noConf;
   }
 
   /**
@@ -526,6 +529,10 @@ public class WollMuxFiles
    */
   public static DatasourceJoiner getDatasourceJoiner()
   {
+    return getDatasourceJoiner(WollMuxSingleton.getInstance().isNoConfig());
+  }
+  public static DatasourceJoiner getDatasourceJoiner(boolean noConfig)
+  {
     if (!djInitialized)
     {
       djInitialized = true;
@@ -570,6 +577,9 @@ public class WollMuxFiles
 
       try
       {
+        if (noConfig)
+          senderSourceStr = de.muenchen.allg.itd51.wollmux.NoConfig.NOCONFIG;
+        
         datasourceJoiner =
           new DatasourceJoiner(getWollmuxConf(), senderSourceStr, getLosCacheFile(),
             getDEFAULT_CONTEXT(), datasourceTimeoutLong);
